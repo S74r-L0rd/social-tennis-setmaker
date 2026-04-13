1c0583b0128343a4c73347135ce7fdcdd3dd61e9
S74r-L0rd <aakashko19998@gmail.com>
2026-04-13 15:52:22 +0800

    Fix: Replaced the biased shuffle by Fisher–Yates shuffle instead

--- server/src/algorithm/scheduler.js
+++ server/src/algorithm/scheduler.js
@@ -7,7 +7,14 @@
  * @returns {Array<any>} A new shuffled array.
  */
 function shuffle(array) {
-  return [...array].sort(() => Math.random() - 0.5);
+  const shuffled = [...array];
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+
+  return shuffled;
 }
 
 /**
